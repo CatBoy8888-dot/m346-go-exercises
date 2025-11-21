e6fd726a685b466ad0c664ca2c887ecf9f6774c2
Max Schaad <max.schaad@infosoft.swiss>
2025-11-21 14:56:39 +0100

    Aufgaben(go-1)

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,15 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	var firstName string = "Max"
+	var lastName string = "Schaad"
+	var dayOfBirth uint8 = 14
+	var monthOfBirth uint8 = 2
+	var yearOfBirth uint16 = 2009
+	var numberOfSiblings uint8 = 0
+	var heightInMeters float32 = 1.8
+	var zodiacSign rune = '\u2652'
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
